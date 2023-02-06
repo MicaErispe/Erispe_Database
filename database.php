1d0298f65cb41053fe1a080b4535eeb21e41af02
MicaErispe <122962246+MicaErispe@users.noreply.github.com>
2023-02-06 18:44:02 +0800

    DatabaseFiles
    these are my database files

---
+++ database.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "MicaErispe";
+$password = "#Uphsl123";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE Erispe";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
